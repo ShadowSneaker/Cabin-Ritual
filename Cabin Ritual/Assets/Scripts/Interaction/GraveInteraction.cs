6724b24c233cdd572000fc9abca12479336b43d3
Herobyron <Byronandrews-salvietto@live.co.uk>
2019-08-09 20:26:44 +0100

    interactions for the cabin and first area complete
    > shovel and bolt cutters interact correctly
    > inetraction with grave on hill works
    > back door to the cabin now works correctly
    > an issue with the inventory ui has been found and ill work on that over the next couple of days

---
+++ Cabin Ritual/Assets/Scripts/Interaction/GraveInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveInteraction : MonoBehaviour
+{
+    public Item ItemRequired;
+
+
+    public void GraveEvent()
+    {
+        Controller temp = FindObjectOfType<Controller>();
+
+        if (temp.ReturnLookingAt())
+        {
+            if (temp.GetPlayerInv().EquipedItem == ItemRequired)
+            {
+
+                GetComponent<InteractableObject>().ScreenText = "sucess";
+                Instantiate(ItemRequired.ItemDropped, new Vector3(ItemRequired.DropXPos, ItemRequired.DropYPos, ItemRequired.DropZpos), Quaternion.identity);
+                temp.GetPlayerInv().RemoveItem(ItemRequired);
+            }
+            else
+            {
+                // this function will change the flavour text of the object to say its locked and may need a key. 
+                GetComponent<InteractableObject>().ScreenText = "i may need a shovel here";
+            }
+        }
+    }
+}
