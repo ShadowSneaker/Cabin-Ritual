2370d70078b322a4ae9959947f38b4da9595c1e5
SoItsMaestro <samuelw1994@hotmail.com>
2019-08-21 15:49:32 +0100

    female zomb

--- Cabin Ritual/Assets/Scripts/Entities/AI/ChaseAI.cs
+++ Cabin Ritual/Assets/Scripts/Entities/AI/ChaseAI.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class ChaseAI : AIBase
 {
+
+    
+
     /// Properties
 
     [Tooltip("The object this AI should move towards")]
     public Transform FollowObject;
 
+   
 
     /// Overridables
 
     private void FixedUpdate()
     {
         Agent.destination = FollowObject.position;
+        
     }
 
     /// Functions
