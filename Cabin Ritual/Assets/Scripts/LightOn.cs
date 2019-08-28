ccc0f180c77368108559f73f2663dedb1402efba
SoItsMaestro <samuelw1994@hotmail.com>
2019-08-28 12:29:30 +0100

    have fun ladies

---
+++ Cabin Ritual/Assets/Scripts/LightOn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOn : MonoBehaviour
+{
+    public GameObject Light;
+    private bool LightActivation;
+
+    void start()
+    {
+        LightActivation = false;
+    }
+    //public void TurnOnLight()
+    //{
+    //    //Controller temp = FindObjectOfType<Controller>();
+    //    //if (temp.ReturnLookingAt())
+    //    //{
+    //    if (Input.GetKey("E"))
+    //    {
+    //        Light.SetActive(true);
+    //    }
+    //    //}
+    //}
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //if (Input.GetButtonDown("E"))
+            //{
+                Debug.Log("running");
+                Light.SetActive(true);
+            //}
+        }
+    }
+}
