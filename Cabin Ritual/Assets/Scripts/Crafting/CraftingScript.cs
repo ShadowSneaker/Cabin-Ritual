f5983bfca352132473c110f8d6ffaac01dd1e2db
Herobyron <Byronandrews-salvietto@live.co.uk>
2019-09-03 20:51:43 +0100

    changes to the craftingscript and added ui script
    > crafting ui script added
    > changes made to the crafting script

--- Cabin Ritual/Assets/Scripts/Crafting/CraftingScript.cs
+++ Cabin Ritual/Assets/Scripts/Crafting/CraftingScript.cs
@@ -6,9 +6,7 @@ using UnityEngine.UI;
 public class CraftingScript : MonoBehaviour
 {
     // notes for next time//
-    // need to make it so the creen appears on opening the crafting
-    // need to put the function open craft menu on to the object in game
-    //the image list needs to find the items automatically in the function (i shoulnd tbe adding them in inspector)
+    // the image list needs to find the items automatically in the function (i shoulnd tbe adding them in inspector)
     // then need to do combining
     // then need to do actual removing of items
     // have the new combined item put in players inventory
@@ -25,11 +23,13 @@ public class CraftingScript : MonoBehaviour
     public List<Item> CraftingItems = new List<Item>();
 
     // list of item images for the differnt items
-    public List<Image> ItemImages = new List<Image>();
+    // this needs moving to the ui part
+    //public List<Transform> ItemImages = new List<Transform>();
 
     public void Awake()
     {
-        CraftingScreen.enabled = false;
+  
+        CraftingScreen.gameObject.SetActive(false);
     }
 
     // a function that makes the crafting menu open 
@@ -43,14 +43,17 @@ public class CraftingScript : MonoBehaviour
         {
             CraftingItems = temp.GetPlayerInv().Items;
 
-            for(int i = 0; i < CraftingItems.Count; ++i)
+           // have a function here form the crafting ui that grabs the list and puts all  the images from that list onto the screen
+
+            for (int i = 0; i < CraftingItems.Count; ++i)
             {
-                ItemImages[i].sprite = CraftingItems[i].Icon;
+               
+
             }
 
         }
 
-        CraftingScreen.enabled = true;
+        CraftingScreen.gameObject.SetActive(true);
 
     }
 
@@ -58,14 +61,6 @@ public class CraftingScript : MonoBehaviour
     // checks wether the two materials supplied can be crafted make something
     // it will remove the items used from the players inventory if sucessfull
     public void Craft()
-    {
-
-    }
-
-
-    // this closes the crafting menu
-    // it will also clear all the slots in the crafting menu when done so that nothing is dragged over
-    public void CloseCrftingMenu()
     {
 
     }
---
+++ Cabin Ritual/Assets/Scripts/Crafting/CraftingUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CraftingUI : MonoBehaviour
+{
+    public Image CraftingUIImage;
+
+
+
+
+    // this closes the crafting menu
+    // it will also clear all the slots in the crafting menu when done so that nothing is dragged over
+    public void CloseCrftingMenu()
+    {
+        CraftingUIImage.gameObject.SetActive(false);
+    }
+
+
+
+
+
+}
