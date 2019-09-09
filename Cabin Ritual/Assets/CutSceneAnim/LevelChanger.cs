b511e0438f83d555cce359a6348f5b06f4ef8c9c
SoItsMaestro <samuelw1994@hotmail.com>
2019-09-09 17:19:30 +0100

    Stuff

---
+++ Cabin Ritual/Assets/CutSceneAnim/LevelChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelChanger : MonoBehaviour
+{
+    public Animator Anim;
+
+    private int LevelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeToLevel(1);
+    }
+
+    void FadeToLevel(int levelIndex)
+    {
+        LevelToLoad = levelIndex;
+        Anim.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+
+    }
+}
