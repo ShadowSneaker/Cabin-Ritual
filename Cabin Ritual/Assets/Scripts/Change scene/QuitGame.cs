574eb0e93e695bfef426421afbedf88883379547
SoItsMaestro <33253668+SoItsMaestro@users.noreply.github.com>
2019-12-17 15:53:38 +0000

    Quit Button

--- Cabin Ritual/Assets/Scripts/Change scene/QuitGame.cs
+++ Cabin Ritual/Assets/Scripts/Change scene/QuitGame.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void QuitG()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();        
     }
 }
